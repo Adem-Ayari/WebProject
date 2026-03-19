8f24632be5dbb0e2ae9d7e907471176b039a4b23
Ahmed El Mustapha Jrad <ahmedelmustapha.jrad@insat.ucar.tn>
2026-03-19 13:02:48 +0100

    added button dashboard into navbar of home

--- login_signup/Login-register.js
+++ login_signup/Login-register.js
@@ -1,16 +1,18 @@
-const wrapper = document.querySelector(" .wrapper");
-const login = document.querySelector(" .form-box-login");
-const register = document.querySelector(" .form-box-register");
-const login_link = document.querySelector(" .login_link");
-const register_link = document.querySelector(" .register_link");
-if (wrapper && login && register && login_link && register_link) {
-  console.log("yes");
+const login = document.querySelector(".form-box-login");
+const register = document.querySelector(".form-box-register");
+const loginLink = document.querySelector(".login_link");
+const registerLink = document.querySelector(".register_link");
+
+if (login && register && loginLink && registerLink) {
+  loginLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    register.style.display = "none";
+    login.style.display = "block";
+  });
+
+  registerLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    login.style.display = "none";
+    register.style.display = "block";
+  });
 }
-login_link.addEventListener("click", () => {
-  register.style.display = "none";
-  login.style.display = "block";
-});
-register_link.addEventListener("click", () => {
-  login.style.display = "none";
-  register.style.display = "block";
-});
